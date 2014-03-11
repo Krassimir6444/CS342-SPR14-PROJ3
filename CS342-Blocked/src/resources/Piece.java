50f7c64b9b94b0e89006ac8c9f64dca0a1035caa
Krassimir <Krassimir@10.44.246.52>
2014-03-11 10:03:09 -0500

    Worked on Piece.java a bit, added wood image to use for blocks

--- CS342-Blocked/src/resources/Piece.java
+++ CS342-Blocked/src/resources/Piece.java
@@ -1,11 +1,41 @@
 package resources;
 
 import java.awt.*;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 
-public class Piece {
+public class Piece extends JButton { 
 	
-	public Piece() {
-		
+	private boolean Z = false;
+	private int width;
+	private int height;
+	private int xPos;
+	private int yPos;
+	
+	public Piece(int w, int h, int x, int y) {
+		super();
+		try {
+		  Image icon = ImageIO.read(getClass().getResource("wood.gif"));
+		  this.setIcon(new ImageIcon(icon));
+		} catch (IOException ex) { }
+		width = w;
+		height = h;
+		xPos = x;
+		yPos = y;
+		this.setVisible(true);
 	}
 	
+	public void setTargetPiece() {
+		this.Z = true;
+	}
+	
+	public int getX() {
+		return this.xPos;
+	}
+
+	public int getY() {
+		return this.yPos;
+	}
 }
